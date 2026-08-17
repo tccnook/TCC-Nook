41509d967b94f194494e99509f0ba645f8d889f8
MiguelUC11 <uchoamiguel384@gmail.com>
2026-08-16 22:12:07 -0300

    pagina config user

--- paginas/config_user.php
+++ paginas/config_user.php
@@ -75,10 +75,10 @@
         <input type="file" name="banner" id="banner" accept="image/jpeg,image/png" hidden>
 
         <label for="nome_completo">Nome</label><br>
-        <input type="text" name="nome_completo" id="nome_completo" placeholder="<?= htmlspecialchars($nome_completo)?>"><br>
+        <input type="text" name="nome_completo" id="nome_completo" value="<?= htmlspecialchars($nome_completo)?>"><br>
 
         <label for="username">Nome de Usuário</label><br>
-        <input type="text" name="username" id="username" placeholder="<?= htmlspecialchars($username)?>"><br>
+        <input type="text" name="username" id="username" value="<?= htmlspecialchars($username)?>"><br>
 
         <label for="bio">Bio</label><br>
         <textarea name="bio"><?= htmlspecialchars($bio ?? '') ?></textarea><br>
@@ -92,7 +92,68 @@
         <button type="submit" name="salvar">Salvar alterações</button>
     
     </form>
-    <?php
+    <section>
+        Email
+        <button type="button" id="btn_alterar_email">
+            Alterar Email
+        </button><br>
+        <dialog id="alterar_email">
+            <h2>Alterar Email</h2>
+
+            <form action="#" method="POST">
+                <label for="email">Email:</label><br>
+                <input type="text" name="email" value="<?= htmlspecialchars($usuario['email'])?>"><br>
+                <button type="submit" name="alterar_email">Alterar Email</button>
+                <button type="button" id="fechar_email">Cancelar</button>
+            </form>
+        </dialog>
+
+
+        Senha
+        <button type="button" id="btn_alterar_senha">
+            Alterar Senha
+        </button>
+        <dialog id="alterar_senha">
+            <h2>Alterar Senha</h2>
+
+            <form action="#" method="POST">
+                <label for="senha">Senha:</label><br>
+                <input type="password" name="senha"><br>
+                <label for="confirmar_senha">Confirmar Senha:</label><br>
+                <input type="password" name="confirmar_senha"><br>
+                <button type="submit" name="alterar_senha">Alterar Senha</button>
+                <button type="button" id="fechar_senha">Cancelar</button>
+            </form>
+        </dialog>
+    </section>
+        <script>
+        const btn_alterar_email = document.getElementById("btn_alterar_email");
+        const fechar_email = document.getElementById("fechar_email");
+        const alterar_email = document.getElementById("alterar_email");
+
+        const btn_alterar_senha = document.getElementById("btn_alterar_senha");
+        const fechar_senha = document.getElementById("fechar_senha");
+        const alterar_senha = document.getElementById("alterar_senha");
+
+        btn_alterar_email.addEventListener("click", function () {
+            alterar_email.showModal();
+        });
+
+        fechar_email.addEventListener("click", function () {
+            alterar_email.close();
+        });
+
+        btn_alterar_senha.addEventListener("click", function () {
+            alterar_senha.showModal();
+        });
+
+        fechar_senha.addEventListener("click", function () {
+            alterar_senha.close();
+        });
+
+
+    </script>
+    <?php //updates nome, username, foto, banner e bio
         if(isset($_POST['salvar'])){
             $foto_perfil = $_FILES['foto_perfil'];
             $banner = $_FILES['banner'];
@@ -103,6 +164,12 @@
 
             if($visibilidade !== 'publico' && $visibilidade !== 'privado'){
                 $visibilidade = 'publico';
+            }
+
+            if (empty($nome_completo) && empty($username) && empty($bio)) {
+                $nome_completo = $usuario['nome_completo'];
+                $username = $usuario['username'];
+                $bio = $conta['bio'];
             }
 
             if (isset($_FILES['foto_perfil']) && $_FILES['foto_perfil']['error'] === UPLOAD_ERR_OK){
@@ -165,6 +232,16 @@
 
             $editar_user = "update usuario set nome_completo = :nome_completo, username = :username where id_user = :id_user;";
 
+            $verif_username = "select * from usuario where username=:username";
+            $stmt = $conn->prepare($verif_username);
+            $stmt ->execute([":username" => $nome_user]);
+
+            if($stmt->fetch()){
+                $_SESSION["erro"] = "Este username já existe!";
+                header("location:config_user.php");
+                exit();
+            }
+
             try{
                 $stmt = $conn->prepare($editar_user);
                 $stmt->execute([
@@ -180,6 +257,71 @@
                 echo "Erro ao atualizar dados: ". $e->getMessage();
             }
         }
+
+        //update email
+        if(isset($_POST['alterar_email'])){
+            $email = $_POST['email'];
+
+            if(empty($email)){
+                $email = $usuario['email'];
+            }
+
+            $verif_email = "select * from usuario where email=:email";
+            $stmt = $conn->prepare($verif_email);
+            $stmt ->execute([":email" => $email]);
+
+            if($stmt->fetch()){
+                $_SESSION["erro"] = "Email já cadastrado!";
+                echo '<script>alert("Email já cadastrado!")</script>';
+                exit();
+            }
+
+            $editar_email = "update usuario set email = :email where id_user = :id_user;";
+
+            try{
+                $stmt = $conn->prepare($editar_email);
+                $stmt->execute([
+                    ":email" => $email,
+                    ":id_user" => $id_user
+                ]);
+                
+                header("Location: " . $_SERVER['PHP_SELF']);
+                exit();
+            } catch (PDOException $e){
+                echo "Erro ao atualizar dados: ". $e->getMessage();
+            }
+        }
+
+        //update senha
+
+        if(isset($_POST['alterar_senha'])){
+            $senha = $_POST['senha'];
+            $confirmar_senha = $_POST['confirmar_senha'];
+
+            if($senha !== $confirmar_senha){
+                $_SESSION["erro"] = "As senhas não coincidem, tente novamente";
+                echo "<script>alert('As senhas não coincidem, tente novamente')</script>";
+                exit();
+            }
+
+            $senha_cripto = password_hash($senha, PASSWORD_DEFAULT);
+
+            $editar_senha = "update usuario set senha = :senha where id_user = :id_user;";
+
+            try{
+                $stmt = $conn->prepare($editar_senha);
+                $stmt->execute([
+                    ":senha" => $senha_cripto,
+                    ":id_user" => $id_user
+                ]);
+                
+                header("Location: " . $_SERVER['PHP_SELF']);
+                exit();
+            } catch (PDOException $e){
+                echo "Erro ao atualizar dados: ". $e->getMessage();
+            }
+        }
+
     ?>
 </body>
 </html>
