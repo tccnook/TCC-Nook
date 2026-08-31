9eb6ff854ed0c864de88d6cabc3538692db76cf1
murillo321 <murillo.oliveira212@gmail.com>
2026-08-30 22:58:19 -0300

    faltou um coisinho

--- paginas/preferencia_gen.php
+++ paginas/preferencia_gen.php
@@ -52,7 +52,7 @@
     <link href="https://fonts.googleapis.com/css2?family=Lora:ital,wght@0,400;0,500;0,600;0,700;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet">
     <link rel="shortcut icon" href="/TCC-Nook/img/icons/ico-nook/ico-nook.ico" type="image/x-icon">
         
-    <title> Gêneros favoritos - Nook </title>
+    <title> Preferências - Nook </title>
 </head>
 <body>
     
